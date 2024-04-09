56f1de9869a18369fdd73fc509c95ff6ccce9efd
fika4life <fistewie@gmail.com>
2024-04-09 09:33:45 +0200

    connects posts and categories to next api and prisma db

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,8 +1,19 @@
 import BlogCard from '@/components/BlogCard';
-import Tag from '@/components/Tag';
-import posts from '@/posts.json';
+import CategoryPills from '@/components/CategoryPills';
 
-const BlogPage = () => {
+const getData = async () => {
+  const res = await fetch(`${process.env.URL}/api/posts/`, {
+    cache: 'no-store'
+  });
+
+  if (!res.ok) {
+    throw new Error('Posts fetch failed');
+  }
+  return res.json();
+};
+
+const BlogPage = async () => {
+  const { posts } = await getData();
   return (
     <div className="container mx-auto flex justify-between ">
       <section className="flex flex-col w-3/4 ">
@@ -14,7 +25,7 @@ const BlogPage = () => {
             <BlogCard
               key={post.id}
               CardTitle={post.title}
-              CardDescription={post.body.slice(0, 60)}
+              CardDescription={post.desc.slice(0, 74)}
               date={post.createdAt}
               image={post.imgUrl}
               category={post.category}
@@ -22,20 +33,7 @@ const BlogPage = () => {
           ))}
         </div>
       </section>
-
-      <aside className=" w-1/4 ml-4 px-6 ">
-        <h3 className="mb-8 text-3xl font-bold text-center text-primary  sm:text-3xl md:text-[28px]">
-          Categories
-        </h3>
-        <div className="flex justify-around flex-wrap gap-y-2">
-          <Tag name={'Finance'}></Tag>
-          <Tag name={'Saving'}></Tag>
-          <Tag name={'Investing'}></Tag>
-          <Tag name={'Dreaming Big'}></Tag>
-          <Tag name={'Accounting'}></Tag>
-          <Tag name={'Taxes'}></Tag>
-        </div>
-      </aside>
+      <CategoryPills></CategoryPills>
     </div>
   );
 };
---
+++ components/CategoryPills.js
@@ -0,0 +1,28 @@
+import Tag from './Tag';
+
+const getData = async () => {
+  const response = await fetch(`${process.env.URL}/api/categories/`, {
+    cache: 'no-store'
+  });
+
+  if (!response.ok) {
+    throw new Error('categories fetch failed');
+  }
+  return response.json();
+};
+
+export default async function CategoryPills() {
+  const { categories } = await getData();
+  return (
+    <aside className=" w-1/4 ml-4 px-6 ">
+      <h3 className="mb-8 text-3xl font-bold text-center text-primary  sm:text-3xl md:text-[28px]">
+        Categories
+      </h3>
+      <div className="flex justify-around flex-wrap gap-y-2">
+        {categories.map((category) => (
+          <Tag key={category.id} name={category.title}></Tag>
+        ))}
+      </div>
+    </aside>
+  );
+}
